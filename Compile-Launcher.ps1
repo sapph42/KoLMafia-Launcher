38cd5c6326ad301d02063a2affc1b72e850fb43a
sapph42 <31635854+sapph42@users.noreply.github.com>
2022-09-25 10:38:31 -0400

    Initial uploads of ps1 files

---
+++ Compile-Launcher.ps1
@@ -0,0 +1,6 @@
+﻿Install-Module -Name ps2exe -Scope CurrentUser
+If (Test-Path .\app_icon64.ico) {
+    Invoke-ps2exe -inputFile .\Launch-KoL.ps1 -outputFile .\Launch-KoL.exe -iconFile .\app_icon64.ico
+} else {
+    Invoke-ps2exe -inputFile .\Launch-KoL.ps1 -outputFile .\Launch-KoL.exe
+}
---
+++ Launch-KoL.ps1
@@ -0,0 +1,70 @@
+﻿#Edit This Based On Where KoL lives
+$installLocation = "$env:USERPROFILE\Documents\KoL"
+
+#Probably don't edit below this line
+
+Function Get-ShellOpenFromExtention {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Extension
+    )
+    begin {
+        if ($Extension -notmatch '\.?(?<Extension>[a-z0-9]{1,3}$)') {
+            return $null
+        }
+        $Extension = -join('.', $Matches.Extension)
+    }
+    process {
+        try {
+            $RegisteredApplication = (Get-ItemProperty "Registry::HKEY_CLASSES_ROOT\$Extension" -ErrorAction SilentlyContinue).'(default)'
+            if ($null -eq $RegisteredApplication) {return $null}
+            $ShellOpen = (Get-ItemProperty "Registry::HKEY_CLASSES_ROOT\$RegisteredApplication\shell\open\command" -ErrorAction SilentlyContinue).'(default)'
+        } catch {
+            return $null
+        }
+        if ($null -eq $ShellOpen) {return $null}
+        $ShellOpen = $ShellOpen.Replace(' %*','')
+        if ($shellOpen -match '(?(^")(?<path>"[^"]*")|(?<path>[^ ]*)) (?<params>.*)') {
+            return @{
+                appPath = $Matches.path
+                arguments = $Matches.params
+            }
+        }
+    }
+    end {
+        return $null
+    }
+}
+
+#Get Registered Application for jar files
+$OpenCommand = Get-ShellOpenFromExtention -Extension '.jar'
+if ($null -eq $OpenCommand) {
+    #Could not pull jar association from registry.  Assume defaults and hope for the best
+    $javaPath = "javaw.exe"
+    $params = "-jar ""%1"""
+} else {
+    $javaPath = $OpenCommand.appPath
+    $params = $OpenCommand.arguments
+}
+#Check kolmafia for name of latest build
+$base = "https://builds.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/"
+Set-Location $installLocation
+$current = gci .\*.jar
+try {
+    $response = Invoke-WebRequest $base
+    $latest = ($response.Links | Select href | ? {$_.href -like "*.jar"}).href
+    #Get Current local build
+    if ($current.Name -ne $latest) {
+        #If the current local build does not match the latest build, remove local build and download latest
+        Invoke-WebRequest $(-join($base, $latest)) -OutFile ".\$latest"
+        Remove-Item $current.FullName
+    }
+} catch {
+    $StatusCode = $_.Exception.Response.StatusCode.value__
+    $StatusDescription = $_.Exception.Response.StatusDescription
+    [System.Windows.Forms.MessageBox]::Show("Received a $StatusCode ($StatusDescription) error when attempting to retreive the latest version.")
+    $latest = $current
+} finally {
+    #Note: In theory Invoke-Item .\$latest should suffice.  However, doing this seems to load KoL without settings data.  So we do it the hard way
+    Start-Process -FilePath $javaPath -ArgumentList $params.Replace("%1", ".\$latest")
+}
