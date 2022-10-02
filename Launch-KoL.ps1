a06c6a2710908bf92b97aa6cb739ed6a822cd1ef
sapph42 <31635854+sapph42@users.noreply.github.com>
2022-10-02 19:58:08 -0400

    Logic to verify download fingerprint and retry

--- Launch-KoL.ps1
+++ Launch-KoL.ps1
@@ -1,16 +1,19 @@
-﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-#Don't Edit This Script
-#Edit Launch-KoL.pref instead
+Add-Type -AssemblyName 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+$scriptPath = $PSScriptRoot
 if (-not (Test-Path $scriptPath\Launch-Kol.pref)) {
     $default = @"
 <?xml version="1.0"?>
-<Location>
-    $scriptPath
-</Location>
+    <Location>$scriptPath</Location>
+    <MaxAttempts>3</MaxAttempts>
 "@
     $default | Out-File -FilePath $scriptPath\Launch-Kol.pref -NoClobber
 }
 $installLocation = (Select-Xml -Path $scriptPath\Launch-KoL.pref -XPath '/Location').Node.InnerText.Trim()
+try {
+    $maxAttempts = (Select-Xml -Path $scriptPath\Launch-KoL.pref -XPath '/MaxAttempts').Node.InnerText.Trim()
+} catch {
+    $maxAttempts = 3
+}
 if ($installLocation -match 'InsertYourKolPath') {
     $installLocation = $scriptPath
 }
@@ -65,22 +68,38 @@ $current = Get-ChildItem .\*.jar
 if ($current.count -gt 1) {
     $current | Sort-Object -Property Name | Select-Object -First 1 | Remove-Item
     $current = $current | Sort-Object -Property Name -Descending | Select-Object -First 1
+    $exists = $true
+    $localFingerprint = (Get-FileHash $current -Algorithm MD5).Hash.ToLower()
 } elseif ($current.Count -eq 0) {
     $message = "No jar file found in the provided folder. Download latest mafia to $($installLocation)?"
     $answer = [System.Windows.Forms.MessageBox]::Show($message,"Mafia Not Found!",[System.Windows.Forms.MessageBoxButtons]::YesNo,[System.Windows.Forms.MessageBoxIcon]::Question,[System.Windows.Forms.MessageBoxDefaultButton]::Button1,0)
     if ($answer -eq [System.Windows.Forms.DialogResult]::No) {
         exit
     } else {
-        $current = @{Name = '1'}
+        $exists = $false
     }
+} else {
+    $exists = $true
+    $localFingerprint = (Get-FileHash $current -Algorithm MD5).Hash.ToLower()
 }
 try {
     $response = Invoke-WebRequest $base
-    $latest = ($response.Links | Select href | ? {$_.href -like "*.jar"}).href
+    $latest = ($response.Links | Select-Object href | Where-Object {$_.href -like "*.jar"}).href
+    $jarURI = -join($base, $latest)
+    $fingerprintURI = -join($jarURI, '/*fingerprint*/')
+    $response = Invoke-WebRequest $fingerprintURI
+    $cannonicalFingerprint = ($response.links | Select-Object href, innerText | Where-Object {$_.href -eq '/' -and $_.innerText -match '[a-z0-9]{32}'}).innerText
     #Get Current local build
-    if ($current.Name -ne $latest) {
+    if ((-not $exists) -or ($current.Name -ne $latest) -or ($localFingerprint -ne $cannonicalFingerprint)) {
         #If the current local build does not match the latest build, remove local build and download latest
-        Invoke-WebRequest $(-join($base, $latest)) -OutFile ".\$latest"
+        Invoke-WebRequest $jarURI -OutFile ".\$latest"
+        $attempts = 1
+        $localFingerprint = (Get-FileHash ".\$latest" -Algorithm MD5).Hash.ToLower()
+        while (($cannonicalFingerprint -ne $localFingerprint) -and ($attempts -le $maxAttempts)) {
+            Remove-Item ".\$latest"
+            Invoke-WebRequest $jarURI -OutFile ".\$latest"
+            $attempts++
+        }
         if ($current.Name -ne '1') {
             Remove-Item $current.FullName
         }
