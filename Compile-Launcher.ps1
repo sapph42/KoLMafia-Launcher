5a56477e31c43f302d0bcac0b1cfe1b6c9b4d7fe
sapph42 <31635854+sapph42@users.noreply.github.com>
2022-10-01 22:51:29 -0400

    Updated to intelligently install or import module

--- Compile-Launcher.ps1
+++ Compile-Launcher.ps1
@@ -1,4 +1,18 @@
-﻿Install-Module -Name ps2exe -Scope CurrentUser
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+if (Get-Module | Where-Object {$_.Name -eq 'ps2exe'}) {
+} else {
+    if (Get-Module -ListAvailable | Where-Object {$_.Name -eq 'ps2exe'}) {
+        Import-Module 'ps2exe' -Verbose
+    } else {
+        if (Find-Module -Name 'ps2exe' | Where-Object {$_.Name -eq 'ps2exe'}) {
+            Install-Module -Name 'ps2exe' -Force -Verbose -Scope CurrentUser
+            Import-Module 'ps2exe' -Verbose
+        } else {
+            EXIT 1
+        }
+    }
+}
+Set-Location $scriptPath
 If (Test-Path .\app_icon64.ico) {
     Invoke-ps2exe -inputFile .\Launch-KoL.ps1 -outputFile .\Launch-KoL.exe -iconFile .\app_icon64.ico
 } else {
