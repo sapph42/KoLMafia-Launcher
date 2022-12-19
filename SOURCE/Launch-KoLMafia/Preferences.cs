833fa9dfe5591f001016fb706bca9a5c85e1d77c
sapph42 <nick.gibson@gmail.com>
2022-12-18 21:51:32 -0500

    Moved WinAPI to separate class

--- SOURCE/Launch-KoLMafia/Preferences.cs
+++ SOURCE/Launch-KoLMafia/Preferences.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Launch_KoLMafia {
--- SOURCE/Launch-KoLMafia/Program.cs
+++ SOURCE/Launch-KoLMafia/Program.cs
@@ -9,8 +9,6 @@ using System.Net.Http;
 using System.Resources;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Windows.Bits;
@@ -18,84 +16,11 @@ using Windows.Bits;
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 namespace Launch_KoLMafia {
 	internal static class Program {
-		[DllImport("Shlwapi.dll", CharSet = CharSet.Unicode)]
-		public static extern uint AssocQueryString(
-			AssocF flags,
-			AssocStr str,
-			string pszAssoc,
-			string? pszExtra,
-			[Out] StringBuilder? pszOut,
-			ref uint pcchOut
-		);
-		[Flags]
-		public enum AssocF {
-			None = 0,
-			Init_NoRemapCLSID = 0x1,
-			Init_ByExeName = 0x2,
-			Open_ByExeName = 0x2,
-			Init_DefaultToStar = 0x4,
-			Init_DefaultToFolder = 0x8,
-			NoUserSettings = 0x10,
-			NoTruncate = 0x20,
-			Verify = 0x40,
-			RemapRunDll = 0x80,
-			NoFixUps = 0x100,
-			IgnoreBaseClass = 0x200,
-			Init_IgnoreUnknown = 0x400,
-			Init_Fixed_ProgId = 0x800,
-			Is_Protocol = 0x1000,
-			Init_For_File = 0x2000
-		}
-		public enum AssocStr {
-			Command = 1,
-			Executable,
-			FriendlyDocName,
-			FriendlyAppName,
-			NoOpen,
-			ShellNewValue,
-			DDECommand,
-			DDEIfExec,
-			DDEApplication,
-			DDETopic,
-			InfoTip,
-			QuickTip,
-			TileInfo,
-			ContentType,
-			DefaultIcon,
-			ShellExtension,
-			DropTarget,
-			DelegateExecute,
-			Supported_Uri_Protocols,
-			ProgID,
-			AppID,
-			AppPublisher,
-			AppIconReference,
-			Max
-		}
-		private const string KoLBaseLocation = @"https://builds.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/";
-		// private const string KoLAPI = @"https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/api/json";
+        private const string KoLBaseLocation = @"https://builds.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/artifact/dist/";
+        // private const string KoLAPI = @"https://ci.kolmafia.us/job/Kolmafia/lastSuccessfulBuild/api/json";
         // artifactJSON.RootElement.GetProperty("artifacts")[0].GetProperty("relativePath").ToString();
         static readonly HttpClient client = new();
-
-        [return: MaybeNull]
-        static string AssocQueryString(AssocStr association, string extension) {
-			const int S_OK = 0;
-			const int S_FALSE = 1;
-
-			uint length = 0;
-			uint ret = AssocQueryString(AssocF.None, association, extension, null, null, ref length);
-			if (ret != S_FALSE) {
-			    return null;
-			}
-
-			StringBuilder sb = new((int)length); // (length-1) will probably work too as the marshaller adds null termination
-			ret = AssocQueryString(AssocF.None, association, extension, null, sb, ref length);
-			if (ret != S_OK) {
-			    return null;
-			}
-
-			return sb.ToString();
-		}
+        
 		[return: NotNull]
 		static bool GetWebFile(Uri URI, 
 								FileInfo Destination, 
@@ -228,7 +153,7 @@ namespace Launch_KoLMafia {
 					preferences.SkippedVersion = releaseVer;
 				}
 			}
-			javaName = AssocQueryString(AssocStr.DDEApplication, ".jar") ?? "";
+			javaName = WinAPI.AssocQueryString(WinAPI.AssocStr.DDEApplication, ".jar") ?? "";
 			if (javaName == "") { killOnUpdate = false; }
 			if (killOnUpdate) {
 				if (!KillAllProcByName(javaName)) {
